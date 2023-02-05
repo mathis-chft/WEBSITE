4b4d64134208cf47ddc0ab3f81c7b2a2a43a2f4d
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-05 20:26:19 +0100

    Meta et amélioration de SEO

--- vue-project/src/components/AboutMe.vue
+++ vue-project/src/components/AboutMe.vue
@@ -1,4 +1,4 @@
 <template>
         <h2 class="mb-10 text-3xl text-white">001. ABOUT ME</h2>
-        <h3 class="text-lg md:text-xl leading-relaxed text-center text-white w-[85%] md:w-1/2 mb-20">Hey there ! I'm <a>Mathis CHOUFFOT</a>, student at <a>Epitech Digital School</a>. I'm all about learning about and working on <a>digital transformation</a>. My school projects cover a wide range of digital topics, from <a>UX/UI</a> design to <a>developement</a> passing by <a>business</a>. Basically, I'm trying to figure out how digital can be used to transform the world.</h3>
+        <h3 class="text-lg md:text-xl leading-relaxed text-center text-white w-[85%] md:w-1/2 mb-20">Hey there ! I'm <a>Mathis CHOUFFOT</a>, student at <a>Epitech Digital School</a>. I'm all about learning about and working on <a>digital transformation</a>. My school projects cover a wide range of digital topics, from <a>UX/UI</a> design to <a>developement</a> passing by <a>business</a>. Basically, I'm trying to figure out how <a>digital</a> can be used to <a>transform the world</a>.</h3>
 </template>
