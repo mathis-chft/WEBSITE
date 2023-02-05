992ab06809576a6a9efa23d4262e5456b11e7d5d
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-05 19:21:26 +0100

    Contact

--- vue-project/src/views/Home.vue
+++ vue-project/src/views/Home.vue
@@ -3,9 +3,14 @@
     <div class="flex flex-col items-center pt-44">
         <AboutMe/>
     </div>
-    <div class="flex flex-col items-center gap-10 mb-16">
-    <h2 class="mb-4 text-3xl text-white">002. MY PROJECTS</h2>
+    
+    <div class="flex flex-col items-center gap-10">
+        <h2 class="mb-4 text-3xl text-white">002. MY PROJECTS</h2>
         <Projects/>
+    </div>
+    
+    <div class="flex flex-col items-center pt-20">
+        <Contact/>
     </div>
 </template>
 
@@ -13,12 +18,14 @@
 import Projects from '../components/Projects.vue';
 import Navbar from '../components/Navbar.vue';
 import AboutMe from '../components/AboutMe.vue';
+import Contact from '../components/Contact.vue'
 
 export default {
     name: 'Home',
     components: {
         Projects,
         Navbar,
+        Contact,
         AboutMe
     }
 }
