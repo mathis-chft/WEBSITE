c34f61143fd4acbd1459787cb31afad9287792ea
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-05 19:40:14 +0100

    margin mail

--- vue-project/src/components/Contact.vue
+++ vue-project/src/components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
         <h2 class="mb-10 text-3xl text-white">003. CONTACT</h2>
         <h3 class="text-lg md:text-xl text-center text-white w-[85%] md:w-1/2 mb-10">Need to <a>contact me</a> ? You can <a>reach me</a> by the following address 👇</h3>
-        <h3 class="mb-20 text-lg text-center text-white md:text-xl md:w-1/2 hover:underline"><a href="mailto: contact@mathischouffot.fr">contact@mathischouffot.fr</a></h3>
+        <h3 class="mb-10 text-lg text-center text-white md:text-xl md:w-1/2 hover:underline"><a href="mailto: contact@mathischouffot.fr">contact@mathischouffot.fr</a></h3>
 </template>
