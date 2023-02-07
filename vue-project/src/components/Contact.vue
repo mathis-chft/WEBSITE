e6da706fd03d5224a081ed9ceb75d2071f592a6d
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-07 19:38:16 +0100

    SEO improvements

--- vue-project/src/components/Contact.vue
+++ vue-project/src/components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
         <h2 class="mb-10 text-3xl text-white">003. CONTACT</h2>
-        <h3 class="text-lg md:text-xl text-center text-white w-[85%] md:w-1/2 mb-10">Need to <a>contact me</a> ? You can <a>reach me</a> by the following address 👇</h3>
+        <h3 class="text-lg md:text-xl text-center text-white w-[85%] md:w-1/2 mb-10">Need to <txt>contact me</txt> ? You can <txt>reach me</txt> by the following address 👇</h3>
         <h3 class="mb-10 text-lg text-center text-white md:text-xl md:w-1/2 hover:underline"><a href="mailto: contact@mathischouffot.fr?subject=[Votre nom]-[Votre demande]&body=Décrivez ici la raison de votre contact.">contact@mathischouffot.fr</a></h3>
 </template>
