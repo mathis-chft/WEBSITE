8b2a20f36a99481d1627c7b86f28dd5c893345c0
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-08 00:54:09 +0100

    optimisations de code

--- vue-project/src/components/Projects.vue
+++ vue-project/src/components/Projects.vue
@@ -1,4 +1,5 @@
 <template>
+    <h2 class="mb-4 text-3xl text-white">002. MY PROJECTS</h2>
     <div class="bg-[url('../assets/EPIDEX.svg')] bg-contain bg-origin-content md:p-16 p-10 bg-center bg-no-repeat hover:transition-opacity duration-300 h-32 md:h-48 w-[85%] md:w-[40%] rounded-[34px] bg-[#0c0c0c] hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2"></div>
     <div class="bg-[url('../assets/YOWL.svg')] bg-contain bg-origin-content md:p-10 p-6 bg-center bg-no-repeat hover:transition-opacity duration-300 h-32 md:h-48 w-[85%] md:w-[40%] rounded-[34px] bg-[#0c0c0c] hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2"></div>
     <div class="bg-[url('../assets/Surcadence..svg')] bg-contain bg-origin-content md:p-10 p-10 bg-center bg-no-repeat hover:transition-opacity duration-300 h-32 md:h-48 w-[85%] md:w-[40%] rounded-[34px] bg-[#0c0c0c] hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2"></div>
--- vue-project/src/views/Home.vue
+++ vue-project/src/views/Home.vue
@@ -3,23 +3,18 @@
     <div class="flex flex-col items-center pt-44">
         <AboutMe/>
     </div>
-    
     <div class="flex flex-col items-center gap-10">
-        <h2 class="mb-4 text-3xl text-white">002. MY PROJECTS</h2>
         <Projects/>
     </div>
-    
     <div class="flex flex-col items-center pt-20">
         <Contact/>
     </div>
 </template>
-
 <script>
 import Projects from '../components/Projects.vue';
 import Navbar from '../components/Navbar.vue';
 import AboutMe from '../components/AboutMe.vue';
 import Contact from '../components/Contact.vue'
-
 export default {
     name: 'Home',
     components: {
