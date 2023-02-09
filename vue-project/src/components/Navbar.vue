16fd4f5a751356fbe5c0a34bb477df5a06f61741
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-10 00:03:39 +0100

    lol

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -3,7 +3,7 @@
         <a href="javascript:window.scrollTo(0,0);">
         <img routerlink="/" class="ml-8 duration-300 hover:scale-110 hover:ease-in-out h-10 w-10 rounded-full hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/face.webp" alt="Photo de profil">
         </a>
-        <h1 class="text-xl text-white">Mathis CHOUFFOT</h1>
+        <h1 class="text-xl text-white">Liam Nisbet</h1>
         <a href="https://www.linkedin.com/in/mathis-chouffot/">
             <img class="mr-8 duration-300 hover:scale-110 hover:ease-in-out min-h-full h-8 w-8 rounded-md hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/linkedin.svg" alt="Logo LinkedIn">
         </a>
