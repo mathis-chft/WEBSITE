aa59fb9b861242e655ba907fe0569d6252b53a30
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-12 22:00:22 +0100

    redirection de pages améliorées

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -1,11 +1,11 @@
 <template>
     <nav class="fixed flex items-center justify-between w-screen h-24 text-center bg-black">
       <a href="#" @click.prevent="scrollToTop">
-        <img routerlink="/" class="ml-8 duration-300 hover:scale-110 hover:ease-in-out h-10 w-10 rounded-full hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/face.webp" alt="Photo de profil">
+        <img routerlink="/" class="ml-8 duration-300 hover:scale-110 hover:ease-in-out h-10 w-10 rounded-full hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/face.webp" alt="Photo de profil de Mathis Chouffot">
       </a>
       <h1 class="text-xl text-white">Mathis CHOUFFOT</h1>
-      <a href="https://www.linkedin.com/in/mathis-chouffot/">
-        <img class="mr-8 duration-300 hover:scale-110 hover:ease-in-out min-h-full h-8 w-8 rounded-md hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/linkedin.svg" alt="Logo LinkedIn">
+      <a href="https://www.linkedin.com/in/mathis-chouffot/" target="_blank">
+        <img class="mr-8 duration-300 hover:scale-110 hover:ease-in-out min-h-full h-8 w-8 rounded-md hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/linkedin.svg" alt="Logo LinkedIn redirigeant vers la page LinkedIn de Mathis Chouffot">
       </a>
     </nav>
   </template>
