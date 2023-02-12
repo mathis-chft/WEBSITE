93ca94232cc4c2dad0e3332b997957a01455a788
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-12 21:38:34 +0100

    smooth scrolling

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -1,11 +1,25 @@
 <template>
     <nav class="fixed flex items-center justify-between w-screen h-24 text-center bg-black">
-        <a href="javascript:window.scrollTo(0,0);">
+      <a href="#" @click.prevent="scrollToTop">
         <img routerlink="/" class="ml-8 duration-300 hover:scale-110 hover:ease-in-out h-10 w-10 rounded-full hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/face.webp" alt="Photo de profil">
-        </a>
-        <h1 class="text-xl text-white">Mathis CHOUFFOT</h1>
-        <a href="https://www.linkedin.com/in/mathis-chouffot/">
+      </a>
+      <h1 class="text-xl text-white">Mathis CHOUFFOT</h1>
+      <a href="https://www.linkedin.com/in/mathis-chouffot/">
         <img class="mr-8 duration-300 hover:scale-110 hover:ease-in-out min-h-full h-8 w-8 rounded-md hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/linkedin.svg" alt="Logo LinkedIn">
-        </a>
+      </a>
     </nav>
-</template>
+  </template>
+  
+  <script>
+  export default {
+    methods: {
+      scrollToTop() {
+        window.scrollTo({
+          top: 0,
+          left: 0,
+          behavior: 'smooth'
+        });
+      }
+    }
+  };
+  </script>
