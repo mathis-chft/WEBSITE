80165749665712329288b49813a09c114d8849cc
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-12 23:53:57 +0100

    hove disabled on mobile

--- vue-project/tailwind.config.js
+++ vue-project/tailwind.config.js
@@ -1,11 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  future: {
-    hoverOnlyWhenSupported: true,
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
-}
 }
