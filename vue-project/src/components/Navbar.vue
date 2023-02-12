35f44700b26d0a7587e4e2e9360a7b5474e91770
matmat <114567128+mat-chft@users.noreply.github.com>
2023-02-12 23:50:49 +0100

    version d'essai du hover

--- vue-project/src/components/Navbar.vue
+++ vue-project/src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav class="fixed flex items-center justify-between w-screen h-24 text-center bg-black">
       <a href="#" @click.prevent="scrollToTop">
-        <img routerlink="/" class="ml-8 duration-300 md:hover:scale-110 hover:hover md:hover:ease-in-out h-10 w-10 rounded-full md:hover:ring-[#00B15A] md:hover:ring-offset-2 md:hover:ring-offset-black md:hover:ring-2 md:hover:outline-offset-2" src="../assets/face.webp" alt="Photo de profil de Mathis Chouffot">
+        <img routerlink="/" class="ml-8 duration-300 hover:scale-110 hover:ease-in-out h-10 w-10 rounded-full hover:ring-[#00B15A] hover:ring-offset-2 hover:ring-offset-black hover:ring-2 hover:outline-offset-2" src="../assets/face.webp" alt="Photo de profil de Mathis Chouffot">
       </a>
       <h1 class="text-xl text-white">Mathis CHOUFFOT</h1>
       <a href="https://www.linkedin.com/in/mathis-chouffot/" target="_blank">
--- vue-project/tailwind.config.js
+++ vue-project/tailwind.config.js
@@ -1,8 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
+  future: {
+    hoverOnlyWhenSupported: true,
   content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {},
   },
   plugins: [],
+}
 }
