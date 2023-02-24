11174eec473d66aff782a23ff1facec49afe19f2
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-02-24 23:10:36 +0100

    CV alt

--- vue-project/src/components/CV.vue
+++ vue-project/src/components/CV.vue
@@ -1,6 +1,6 @@
 <template>
     <h2 class="mb-10 text-3xl text-white">003. CV</h2>
         <a href="../../CV.pdf" target="_blank">
-            <img class=" duration-300 rounded-lg md:hover:ring-[#00B15A] md:hover:ring-offset-2 md:hover:ring-offset-black md:hover:ring-2 md:hover:outline-offset-2" src="../../CV.pdf" width="297.5" height="421" scale="tofit"/>
+            <img class=" duration-300 rounded-lg bg-cover md:hover:ring-[#00B15A] md:hover:ring-offset-2 md:hover:ring-offset-black md:hover:ring-2 md:hover:outline-offset-2" src="../../CV.pdf" alt="CV de Mathis Chouffot" width="297.5" height="421" scale="tofit"/>
         </a>
 </template>
