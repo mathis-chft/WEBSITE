364d3eed284854b263b304499ece82578134af40
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-02-24 16:54:15 +0100

    aborted 2

--- vue-project/src/components/CV.vue
+++ vue-project/src/components/CV.vue
@@ -1,6 +1,6 @@
 <template>
     <h2 class="mb-10 text-3xl text-white">003. CV</h2>
         <a href="../../CV de Mathis Chouffot.pdf">
-            <iframe class="duration-300 md:hover:ease-in-out rounded-lg md:hover:ring-[#00B15A] md:hover:ring-offset-2 md:hover:ring-offset-black md:hover:ring-2 md:hover:outline-offset-2" src="../../CV de Mathis Chouffot.pdf" width="297.5" height="421" scale="tofit"/>
+            <iframe class=" duration-300 rounded-lg md:hover:scale-110 md:hover:ring-[#00B15A] md:hover:ring-offset-2 md:hover:ring-offset-black md:hover:ring-2 md:hover:outline-offset-2" src="../../CV.pdf" width="297.5" height="421" scale="tofit"></iframe>
         </a>
 </template>
