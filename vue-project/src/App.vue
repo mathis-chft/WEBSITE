08181d700f2132764b2ce00476699c66b828ddfe
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-04-21 01:42:14 +0200

    Test 2

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -11,7 +11,7 @@ import { RouterLink, RouterView } from "vue-router";
 <script>
 app.use(function (req, res, next) {
   res.setHeader(
-    'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+    'Content-Security-Policy', "default-src 'self'"
   );
   
   next();
