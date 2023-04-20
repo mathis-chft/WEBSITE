b28fbd0552452276cbabfc959814d495998fdc4d
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-04-21 01:57:21 +0200

    CSP gpt

---
+++ vue-project/server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const path = require('path');
+const crypto = require('crypto');
+
+const app = express();
+
+// Middleware pour ajouter le CSP dans les headers HTTP
+app.use((req, res, next) => {
+  const nonce = crypto.randomBytes(16).toString('base64');
+  res.locals.nonce = nonce;
+  res.setHeader('Content-Security-Policy', `default-src 'self'; script-src 'self' 'nonce-${nonce}' 'strict-dynamic'; style-src 'self' 'nonce-${nonce}'; img-src 'self' data:; font-src 'self'; connect-src 'self'; object-src 'none'; frame-src 'self'; base-uri 'self'; form-action 'self'; report-uri https://<YOUR_DOMAIN>/csp-report;`);
+  next();
+});
+
+// Servir les fichiers statiques de l'application Vue.js
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Rediriger toutes les requêtes vers l'application Vue.js
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,13 +7,3 @@ import { RouterLink, RouterView } from "vue-router";
     <RouterView/>
   </body>
 </template>
-
-<script>
-app.use(function (req, res, next) {
-  res.setHeader(
-    'Content-Security-Policy', "default-src 'self'"
-  );
-  
-  next();
-});
-</script>
