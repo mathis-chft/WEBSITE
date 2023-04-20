52f20f5b7f36d6b4e0042c341836edd8cff57219
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-04-21 01:40:29 +0200

    Test CSP header

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -7,3 +7,13 @@ import { RouterLink, RouterView } from "vue-router";
     <RouterView/>
   </body>
 </template>
+
+<script>
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
+  );
+  
+  next();
+});
+</script>
