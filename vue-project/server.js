a6d3b445c0dd7212925ac43237e6d880382c8161
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-04-21 02:31:59 +0200

    Test 6

--- vue-project/server.js
+++ vue-project/server.js
@@ -1,3 +1,8 @@
+const express = require('express');
+const app = express();
+const port = 3000;
+
+
 app.use(function (req, res, next) {
     res.setHeader(
       'Content-Security-Policy-Report-Only', "default-src 'self'; script-src 'self'; style-src 'self'; font-src 'self'; img-src 'self'; frame-src 'self'"
