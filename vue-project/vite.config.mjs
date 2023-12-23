a84c6c9521d587f0a2f13bb7c49fe1b2f1236ec0
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-12-23 18:19:53 +0100

    Add warmup configuration to vite.config.mjs

--- vue-project/vite.config.mjs
+++ vue-project/vite.config.mjs
@@ -5,6 +5,14 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    warmup: {
+      clientFiles: [
+        './src/views/UX-UI.vue',
+        './src/components/Projects.vue',
+      ],
+    },
+  },
   plugins: [vue()],
   resolve: {
     alias: {
