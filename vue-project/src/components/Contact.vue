a60ce313efba3dc1d29d30df470a3ac937e66bb9
matmat <114567128+mathis-chft@users.noreply.github.com>
2023-12-27 21:18:58 +0100

    bigger text

--- vue-project/src/components/Contact.vue
+++ vue-project/src/components/Contact.vue
@@ -1,5 +1,5 @@
 <template>
         <h2 class="mb-4 text-3xl text-white">003. CONTACT</h2>
-        <h3 class="text-lg md:text-xl text-center text-white w-[85%] md:w-1/2 mb-5">Besoin de <span> me contacter</span> ? Vous pouvez me <span>joindre</span> à l'adresse suivante 👇</h3>
-        <h3 class="mb-3 text-lg text-center text-white duration-300 sm:mb-10 hover:scale-110 hover:ease-in-out md:text-xl md:w-1/2 hover:underline"><a href="mailto: contact@mathischouffot.fr?subject=[Votre nom]-[Votre demande]&body=Décrivez ici la raison de votre contact.">contact@mathischouffot.fr</a></h3>
+        <h3 class="text-2xl md:text-2xl text-center text-white w-[85%] md:w-1/2 mb-5">Besoin de <span> me contacter</span> ? Vous pouvez me <span>joindre</span> à l'adresse suivante 👇</h3>
+        <h3 class="mb-3 text-2xl text-center text-white duration-300 sm:mb-10 hover:scale-110 hover:ease-in-out md:text-2xl md:w-1/2 hover:underline"><a href="mailto: contact@mathischouffot.fr?subject=[Votre nom]-[Votre demande]&body=Décrivez ici la raison de votre contact.">contact@mathischouffot.fr</a></h3>
 </template>
